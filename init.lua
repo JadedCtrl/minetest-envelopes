1151a339bc01862dbf978bd9ab37c7e324aed417
Jaidyn Ann <10477760+JadedCtrl@users.noreply.github.com>
2023-12-11 01:33:45 -0600

    Add translation support

--- init.lua
+++ init.lua
@@ -1,20 +1,23 @@
+-- For translation
+local S = minetest.get_translator("envelopes")
+
 minetest.register_craftitem("envelopes:envelope_blank", {
-    description = "Blank Envelope",
+    description = S("Blank Envelope"),
     inventory_image = "envelopes_envelope_blank.png",
     on_use = function(itemstack, user, pointed_thing)
         minetest.show_formspec(user:get_player_name(), "envelopes:input", 
             "size[5.5,5.5]" ..
-            "field[2,0.5;3.5,1;addressee;Addressee;]" ..
-            "label[0,0;Write a letter]" ..
-            "textarea[0.5,1.5;5,3;text;Text;]" ..
-            "field[3,4.8;2.5,1;attn;Attn. (Optional);]" ..
-            "button_exit[0.25,4.5;2,1;exit;Seal]")
+            "field[2,0.5;3.5,1;addressee;" .. S("Addressee") .. ";]" ..
+            "label[0,0;" .. S("Write a letter") .. "]" ..
+            "textarea[0.5,1.5;5,3;text;" .. S("Text") .. ";]" ..
+            "field[3,4.8;2.5,1;attn;" .. S("Attn. (Optional)") .. ";]" ..
+            "button_exit[0.25,4.5;2,1;exit;" .. S("Seal") .. "]")
         return itemstack
     end
 })
 
 minetest.register_craftitem("envelopes:envelope_sealed", {
-    description = "Sealed Envelope",
+    description = S("Sealed Envelope"),
     inventory_image = "envelopes_envelope_sealed.png",
     stack_max = 1,
     groups = {not_in_creative_inventory = 1},
@@ -26,22 +29,24 @@ minetest.register_craftitem("envelopes:envelope_sealed", {
             open_meta:set_string("sender", meta:get_string("sender"))
             open_meta:set_string("receiver", meta:get_string("receiver"))
             open_meta:set_string("text", meta:get_string("text"))
-            local desc = ("Opened Envelope\nTo: " .. meta:get_string("receiver") .. "\nFrom: " .. meta:get_string("sender"))
+            local desc = S("Opened Envelope") .. "\n" ..
+                S("To: @1", meta:get_string("receiver")) .. "\n" ..
+                S("From: @1", meta:get_string("sender"))
             open_meta:set_string("description", desc)
             if meta:get_string("attn") ~= "" then
                 open_meta:set_string("attn", meta:get_string("attn"))
-                desc = desc .. "\nAttn: " .. meta:get_string("attn")
+                desc = desc .. "\n" .. S("Attn: @1", meta:get_string("attn"))
                 open_meta:set_string("description", desc)
             end
             return open_env
         end
-        minetest.chat_send_player(user:get_player_name(), "The seal can only be opened by the addressee!")
+        minetest.chat_send_player(user:get_player_name(), S("The seal can only be opened by the addressee!"))
         return itemstack
     end
 })
 
 minetest.register_craftitem("envelopes:envelope_opened", {
-    description = "Opened Envelope",
+    description = S("Opened Envelope"),
     inventory_image = "envelopes_envelope_opened.png",
     stack_max = 1,
     groups = {not_in_creative_inventory = 1},
@@ -53,11 +58,11 @@ minetest.register_craftitem("envelopes:envelope_opened", {
         local attn = meta:get_string("attn") or ""
         local form = 
             "size[5,5]" ..
-            "label[0,0;A letter from " .. sender .. " to " .. receiver
+            "label[0,0;" .. S("A letter from @1 to @2", sender, receiver)
         if attn ~= "" then
-            form = form .. "\nAttn: " .. attn
+            form = form .. "\n" .. S("Attn: @1", attn)
         end
-        form = form .. "\n" .. text .. "]" .. "button_exit[0,4;2,1;exit;Close]"
+        form = form .. "\n" .. text .. "]" .. "button_exit[0,4;2,1;exit;" .. S("Close") .. "]"
         minetest.show_formspec(user:get_player_name(), "envelope:display", form)
     end
 })
@@ -68,7 +73,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     end
 
     if fields.addressee == "" or fields.addressee == nil or fields.text == "" or fields.text == nil then
-        minetest.chat_send_player(player:get_player_name(), "Please fill out all required fields.")
+        minetest.chat_send_player(player:get_player_name(), S("Please fill out all required fields."))
         return true
     end
 
@@ -81,12 +86,14 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     meta:set_string("receiver", fields.addressee)
     meta:set_string("text", fields.text)
 
-    local desc = ("Sealed Envelope\nTo: " .. fields.addressee .. "\nFrom: " .. player:get_player_name())
+    local desc = S("Sealed Envelope") .. "\n" ..
+        S("To: @1", fields.addressee) .. "\n" ..
+        S("From: @1", player:get_player_name())
     meta:set_string("description", desc)
 
     if fields.attn ~= "" then
         meta:set_string("attn", fields.attn)
-        desc = desc .. "\nAttn: " .. fields.attn
+        desc = desc .. "\n" .. S("Attn: @1", fields.attn)
         meta:set_string("description", desc)
     end
 
@@ -94,7 +101,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
         inv:add_item("main", letter)
         inv:remove_item("main", blank)
     else
-        minetest.chat_send_player(player:get_player_name(), "Unable to create letter! Check your inventory space.")
+        minetest.chat_send_player(player:get_player_name(), S("Unable to create letter! Check your inventory space."))
     end
 
     return true
